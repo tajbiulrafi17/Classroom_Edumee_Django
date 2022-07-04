97cee75bee8f0ca5c752e2267e71f3a8f50ff16a
Tajbiul Hassan Rafi <tajbiul.rafi@northsouth.edu>
2022-07-04 11:54:33 +0600

    Create classroom completed

--- edumee/classroom/urls.py
+++ edumee/classroom/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('class/<int:id>/', views.ClassDashboard.as_view(), name='class_dash')
+    path('class/<int:id>/', views.ClassDashboard.as_view(), name='class_dash'),
+    path('create_class/', views.CreateClass.as_view(), name='create_class')
 ]
--- edumee/classroom/views.py
+++ edumee/classroom/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import get_object_or_404, render
+import imp
+from operator import methodcaller
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from .models import Classroom
+from django.contrib import messages
 
 # Create your views here.
 
@@ -17,3 +20,21 @@ class ClassDashboard(View):
             'room':room,
         }
         return render(request, 'classroom/class_dashboard.html', context)
+
+class CreateClass(View):
+    @method_decorator(login_required(login_url='teacher_login'))
+    def dispatch(self,request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self,request):
+        name = request.POST.get('name')
+        details = request.POST.get('details')
+        name_check = Classroom.objects.filter(name = name)
+        if name_check:
+            messages.warning(request, 'Sorry! Class with this name exists. Try another name.')
+            return redirect('create_class')
+        user = request.user.teachers
+        room = Classroom(teacher=user, name=name, details=details)
+        room.save()
+        messages.success(request, 'Classroom has been Created !!')
+        return redirect('class_dash', id=room.id)
